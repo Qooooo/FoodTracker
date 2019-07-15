2180298c3d561db08d0e9b1e0041f493f5001e3b
Goh Wei Rong <g-w-r@hotmail.com>
2019-07-15 22:14:00 +0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Goh Wei Rong on 15/7/19.
+//  Copyright © 2019 Goh Wei Rong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
